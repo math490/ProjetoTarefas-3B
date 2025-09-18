0ab8fbdde91ba510251f95aa6481b58213bf9e7c
Matheus Teles de Andrade <2023105070008@aluno.ifro.edu.br>
2025-09-18 09:34:09 -0400

    Update: conclusao do CRUD

--- app.py
+++ app.py
@@ -52,7 +52,10 @@ def load_user(user_id):
 def index():
     return render_template("index.html")
 
+#-----------------------------------
 # Cadastro de Usuário --- CREATE
+#-----------------------------------
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -98,12 +101,67 @@ def logout():
     logout_user
     return redirect(url_for("index"))
 
+#-----------------------------------
 # Listar Tarefas --- READ
+#-----------------------------------
+
 @app.route("/tasks", methods=["POST", "GET"])
 @login_required
 def tasks():
     user_tasks = Task.query.filter_by(user_id=current_user.id).all()
     return render_template("tasks.html", tasks=user_tasks)
+
+#-----------------------------------
+# Adicionar Tarefas --- CREATE
+#-----------------------------------
+
+@app.route("/add_tasks", methods=["GET", "POST"])
+@login_required
+def add_tasks():
+    if request.method == "POST":
+        title = request.form.get("title")
+        
+        new_task = Task(title=title, user_id=current_user.id)
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        flash("Tarefa adicionada com sucesso!", "success")
+        return redirect(url_for("tasks"))
+    return render_template("add_tasks.html")
+
+#-----------------------------------
+# Atualizar Status da Tarefa --- UPDATE
+#-----------------------------------
+@app.route("/update_task/<int:id>")
+@login_required
+def update_task(id):
+    task = Task.query.get_or_404(id)
+
+    if task.user_id != current_user.id:
+        flash("Acesso não autorizado - Esta não é a conta logada!", "danger")
+        return redirect(url_for("tasks"))
+    
+    task.status = "Concluída" if task.status == "Pendente" else "Pendente"
+    db.session.commit()
+    return redirect(url_for("tasks"))
+
+#-----------------------------------
+# Deletar Tarefa --- DELETE
+#-----------------------------------
+@app.route("/delete_task/<int:id>")
+@login_required
+def delete_task(id):
+    task = Task.query.get_or_404(id)
+
+    if task.user_id != current_user.id:
+        flash("Acesso não autorizado - Esta não é a conta logada!", "danger")
+        return redirect(url_for("tasks"))
+    
+    db.session.delete(task)
+    db.session.commit()
+    flash("Tarefa excluída com sucesso!", "info")
+    return redirect(url_for("tasks"))
 
 #-----------------------------------
 # CRIAR BANCO NA PRIMEIRA EXECUÇÃO
